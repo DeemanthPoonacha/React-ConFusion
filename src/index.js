78e2fdda10a0af7e6ffcbe4c79a12b382d9a05ee
DeemanthKP <deemanth.p@tataelxsi.co.in>
2022-07-15 16:23:31 +0530

    React component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Menu from './components/MenuComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Menu/>
   </React.StrictMode>
 );
 
